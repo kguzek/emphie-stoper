3d95a6ab1429c38328077079a1541866e57f1677
Konrad (PC) <konrad@guzek.uk>
2023-02-16 20:19:04 +0100

    separate buttons to own component
    implement UI from figma design

---
+++ src/components/Button.svelte
@@ -0,0 +1,30 @@
+<style lang="less">
+	.btn {
+		font-weight: bold;
+		color: white;
+		padding: 10px;
+		border: none;
+		border-radius: 20px;
+			background-color: rgb(87, 140, 255);
+		cursor: pointer;
+		width: 150px;
+		text-transform: uppercase;
+
+		&.stop {
+			background-color: #eb5858;
+		}
+
+		&[disabled] {
+			cursor: not-allowed;
+			background-color: grey;
+		}
+	}
+</style>
+<script lang="ts">
+	export let label: string;
+	export let className: string = "";
+	export let disabled: boolean = false;
+</script>
+<button on:click class={`btn ${className}`} disabled={disabled}>
+	<b>{label}</b>
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,72 +1,121 @@
 <style lang="less">
+	@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:wght@300&display=swap');
+
 	:global(body) {
-		display: flex;
-		justify-content: center;
-		background-color: lightgrey;
-		padding-top: 10px;
+		background-color: #eaeaea;
+		font-family: 'Bebas Neue', cursive;
+		font-family: 'Poppins', sans-serif;
+		color: #444;
 	}
 	.container {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		padding: 15px;
-		background-color: white;
 	}
+
+	.stopwatch {
+		justify-content: center;
+		height: 100%;
+		min-width: 500px;
+
+		div {
+			width: 400px;
+		}
+		
+		.current-reading {
+			font-size: xxx-large;
+			margin-left: 6rem;
+		}
+
+		.buttons {
+			flex-direction: row;
+			justify-content: center;
+			gap: 10px;
+		}
+	}
+	
 	aside {
-		position: absolute;
-		top: 0px;
-		left: 0px;
-		min-width: 200px;
-		border-bottom-right-radius: 15px;
+		margin: -15px -20px;
+		min-width: 250px;
+		border-top-left-radius: 15px;
+		border-bottom-left-radius: 15px;
+		color: grey;
+
+		&.visible {
+			background-color: rgb(213, 228, 248);
+		}
 
 		h3 {
 			margin: 10px;
 		}
+
+		ol {
+			display: flex;
+			flex-direction: column;
+			gap: 5px;
+			padding: 0px;
+			width: 80%;
+			list-style-position: inside;
+
+			.time {
+				background-color: white;
+				padding: 5px 10px;
+				border-radius: 5px;
+
+				b {
+					margin-left: 10px;
+				}
+				
+				button {
+					border: none;
+					float: right;
+					background-color: transparent;
+					color: red;
+					cursor: pointer;
+					display: flex;
+					height: 100%;
+					align-items: center;
+				}
+			}
+		}
 	}
+
+	.root {
+		position: absolute;
+		top: 0px;
+		left: 0px;
+		height: 100vh;
+		width: 100vw;
+		justify-content: center;
+	}
+	
 	main {
-		min-width: 40%;
+		display: flex;
+		justify-content: space-between;
+		background-color: white;
+		width: 70%;
+		height: 70%;
+		gap: 10px;
+		padding: 15px 20px;
 		border-radius: 15px;
-	}
-
-	.btn {
-		padding: 5px;
-		border: 2px solid grey;
-		border-radius: 10px;
-		background-color: lightgrey;
-		cursor: pointer;
-		width: 100px;
-
-		&.start {
-			background-color: lightgreen;
-		}
-
-		&.stop {
-			background-color: #ff9999;
-		}
-
-		&[disabled] {
-			cursor: not-allowed;
-			background-color: #e5e5e5;
-			border-color: #aaa;
-			color: grey;
-		}
-	}
-	.time {
-		button {
-			border: none;
-			margin-left: 0.5rem;
-		}
+		box-shadow: 0px 0px 5px lightgrey;
 	}
 </style>
 <script lang="ts">
 	import { onMount } from "svelte";
+	import Button from "../components/Button.svelte"
 
 	let formattedTime = "00:00:00";
   let stopwatchMilliseconds = "000";
 	let startTime: number;
 	let stopwatchStarted = false;
 
-	let times: {primary: string; secondary: string}[] = [];
+	let times: {primary: string; secondary: string}[] = [
+		{
+			primary: "12:34:56",
+			secondary: "789",
+		},
+	];
 
 	const divMod = (dividend: number, divisor: number) => [Math.floor(dividend / divisor), dividend % divisor];
 
@@ -107,31 +156,40 @@
 		tickTime();
 	});
 </script>
-<aside class="container">
-	<h3>Pomiary</h3>
-	{#each times as time, idx}
-		<div class="time">
-			{time.primary}
-			<small>{time.secondary}</small>
-			<button on:click={() => deleteTime(idx)}>x</button>
+<div class="root container">
+
+	<main>
+		<aside class={"container" + (times.length > 0 ? " visible" : "")}>
+			<h3>{times.length > 0 ? "Zapisane czasy" : ""}</h3>
+			<ol>
+				{#each times as time, idx}
+				<li class="time">
+						<b>{time.primary}</b>
+						<small>{time.secondary}</small>
+					<button on:click={() => deleteTime(idx)}>X</button>
+				</li>
+				{/each}
+			</ol>
+		</aside>
+		<div class="stopwatch container">
+			<div>
+				<div class="current-reading">
+					<b>{formattedTime}</b>
+					<small>{stopwatchMilliseconds}</small>
+				</div>
+				<div class="buttons container">
+					<Button
+					on:click={toggleStopwatch}
+					className={stopwatchStarted ? "stop" : "start"}
+					label={stopwatchStarted ? "Stop" : "Start"}
+					/>
+					<Button on:click={handleTime} disabled={!stopwatchStarted} label="Pomiar" />
+				</div>
+			</div>
 		</div>
-	{/each}
-	{#if times.length === 0}
-		<i>Brak pomiarów</i>
-	{/if}
-</aside>
-<main class="container">
-	<div class="stopwatch">
-		{formattedTime}
-		<small>{stopwatchMilliseconds}</small>
-	</div>
-	<div class="buttons">
-		<button on:click={toggleStopwatch} class={stopwatchStarted ? "btn stop" : "btn start"}>
-			{stopwatchStarted ? "Stop" : "Start"}
-		</button>
-		<button on:click={handleTime} class="btn" disabled={!stopwatchStarted}>
-			Pomiar
-		</button>
-	</div>
-</main>
+		<div class="clock">
+			<b>{new Date().toLocaleTimeString([], {hour: "2-digit", minute: "2-digit", second: "2-digit"})}</b>
+		</div>
+	</main>
+</div>
 
