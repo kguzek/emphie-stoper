09015cba2ffbb2d395f4404c4bc722bf5645aa47
Konrad (PC) <konrad@guzek.uk>
2023-02-18 19:53:36 +0100

    visual fixes

--- _app/immutable/chunks/2-711aab09.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-965732f2.js";export{t as component};
---
+++ _app/immutable/chunks/2-e6902f83.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-2da03c24.js";export{t as component};
