a321bb98c3b9b9d7fce6e11e37fa617bb5c9fb88
Konrad (PC) <konrad@guzek.uk>
2023-02-18 17:13:12 +0100

    implement icons & animations

---
+++ src/components/Icon.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	// Workarounds since I cannot find the actual type declarations
+	interface HTMLProps<_1, _2> {}
+	interface SVGAttributes<_> {}
+
+	export let dimensions = 36;
+
+	// This explicit type declaration is necessary because otherwise there is a type mismatch when inserting `svg.attributes`.
+	interface SVGDefinition {
+		[name: string]: { attributes: HTMLProps<'svg', SVGAttributes<any>>; svg: string };
+	}
+
+	const svgs: SVGDefinition = {
+		timer: {
+			attributes: {},
+			svg: `<path fill="none" d="M0 0h24v24H0z" /><path d="M17.618 5.968l1.453-1.453 1.414 1.414-1.453 1.453a9 9 0 1 1-1.414-1.414zM12 20a7 7 0 1 0 0-14 7 7 0 0 0 0 14zM11 8h2v6h-2V8zM8 1h8v2H8V1z" fill="#808080" />`
+		},
+		cross: {
+			attributes: {
+				stroke: '#f55',
+				'stroke-width': '2',
+				'stroke-linecap': 'round',
+				'stroke-linejoin': 'round'
+			},
+			svg: `<line x1="18" y1="6" x2="6" y2="18" /><line x1="6" y1="6" x2="18" y2="18" />`
+		}
+	};
+	export let name: string & keyof typeof svgs;
+	$: svg = svgs[name];
+</script>
+
+<svg
+	class={name}
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+	width={dimensions}
+	height={dimensions}
+	{...svg.attributes}
+>
+	{@html svg.svg}
+</svg>
+
+<style>
+	svg {
+		position: absolute;
+		transition: opacity 500ms;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,124 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Button from '../components/Button.svelte';
+	import Icon from '../components/Icon.svelte';
+	import BtnTrash from '../components/BtnTrash.svelte';
+
+	let formattedTime = '00:00:00';
+	let stopwatchMilliseconds = '000';
+	let currentTime = '';
+	let startTime: number;
+	let stopwatchStarted = false;
+	let sidebarElement: Element;
+
+	let times: { primary: string; secondary: string }[] = [
+		// {
+		// 	primary: '12:34:56',
+		// 	secondary: '789'
+		// }
+	];
+
+	const divMod = (dividend: number, divisor: number) => [
+		Math.floor(dividend / divisor),
+		dividend % divisor
+	];
+
+	function formatTime(milliseconds: number) {
+		let hours, minutes, seconds;
+		[seconds, milliseconds] = divMod(milliseconds, 1000);
+		[minutes, seconds] = divMod(seconds, 60);
+		[hours, minutes] = divMod(minutes, 60);
+		const parts = [hours, minutes, seconds].map((part) => `${part}`.padStart(2, '0'));
+		formattedTime = parts.join(':');
+		stopwatchMilliseconds = `${milliseconds}`.padStart(3, '0');
+	}
+
+	function tickTime() {
+		const now = new Date();
+		currentTime = now.toLocaleTimeString([], {
+			hour: '2-digit',
+			minute: '2-digit',
+			second: '2-digit'
+		});
+		if (stopwatchStarted) {
+			const timeElapsed = now.getTime() - startTime;
+			formatTime(timeElapsed);
+		}
+		requestAnimationFrame(tickTime);
+	}
+
+	function toggleStopwatch() {
+		stopwatchStarted = !stopwatchStarted;
+		if (stopwatchStarted) {
+			startTime = new Date().getTime();
+		}
+	}
+
+	function toggleSidebar() {
+		if (sidebarElement.classList.contains('hidden')) {
+			sidebarElement.classList.remove('hidden');
+		} else {
+			sidebarElement.classList.add('hidden');
+		}
+	}
+
+	function handleTime() {
+		if (!stopwatchStarted) return;
+		const newTime = { primary: formattedTime, secondary: stopwatchMilliseconds };
+		times = [...times, newTime];
+	}
+
+	const deleteTime = (idxToDelete: number) =>
+		(times = times.filter((_, idx) => idx !== idxToDelete));
+
+	onMount(() => {
+		tickTime();
+	});
+</script>
+
+<div class="root container">
+	<main>
+		<aside class="container hidden" bind:this={sidebarElement}>
+			<button on:click={toggleSidebar} class="btn-toggle-sidebar">
+				<Icon name="timer" />
+				<Icon name="cross" />
+			</button>
+			<h3>Zapisane czasy</h3>
+			<ol>
+				{#each times as time, idx}
+					<li class="time">
+						<div>
+							<b>{time.primary}</b>
+							<small>{time.secondary}</small>
+						</div>
+						<BtnTrash on:click={() => deleteTime(idx)} />
+					</li>
+				{/each}
+			</ol>
+		</aside>
+		<div class="stopwatch container">
+			<div class="current-reading">
+				<b>{formattedTime}</b>
+				{#if formattedTime + stopwatchMilliseconds !== '00:00:00000'}
+					<small>{stopwatchMilliseconds}</small>
+				{/if}
+			</div>
+			<div class="buttons container">
+				<Button
+					on:click={toggleStopwatch}
+					className={stopwatchStarted ? 'stop' : 'start'}
+					label={stopwatchStarted ? 'Stop' : 'Start'}
+				/>
+				<Button on:click={handleTime} disabled={!stopwatchStarted} label="Pomiar" />
+			</div>
+		</div>
+		<b class="clock">{currentTime}</b>
+	</main>
+</div>
+
 <style lang="less">
-  @import url('https://fonts.googleapis.com/css2?family=Poppins&family=Roboto+Mono:wght@700&display=swap');
-	
+	@import url('https://fonts.googleapis.com/css2?family=Poppins&family=Roboto+Mono:wght@700&display=swap');
+
 	:global(body) {
 		background-color: #eaeaea;
 		font-family: 'Bebas Neue', cursive;
@@ -14,15 +132,16 @@
 	}
 
 	.stopwatch {
-		justify-content: center;
+		width: 100%;
 		height: 100%;
+		justify-content: center;
 
 		.current-reading {
-			position: relative;			
+			position: relative;
 			font-size: xxx-large;
 
 			b {
-			  font-family: 'Roboto Mono', monospace;
+				font-family: 'Roboto Mono', monospace;
 			}
 
 			small {
@@ -38,52 +157,6 @@
 			gap: 10px;
 		}
 	}
-	
-	aside {
-		margin: -15px -20px;
-		min-width: 250px;
-		border-top-left-radius: 15px;
-		border-bottom-left-radius: 15px;
-		color: grey;
-
-		&.visible {
-			background-color: rgb(213, 228, 248);
-		}
-
-		h3 {
-			margin: 10px;
-		}
-
-		ol {
-			display: flex;
-			flex-direction: column;
-			gap: 5px;
-			padding: 0px;
-			width: 80%;
-			list-style-position: inside;
-
-			.time {
-				background-color: white;
-				padding: 5px 10px;
-				border-radius: 5px;
-
-				b {
-					margin-left: 10px;
-				}
-				
-				button {
-					border: none;
-					float: right;
-					background-color: transparent;
-					color: red;
-					cursor: pointer;
-					display: flex;
-					height: 100%;
-					align-items: center;
-				}
-			}
-		}
-	}
 
 	.root {
 		position: absolute;
@@ -93,113 +166,97 @@
 		width: 100vw;
 		justify-content: center;
 	}
-	
+
 	main {
 		display: flex;
-		justify-content: space-between;
+		position: relative;
 		background-color: white;
 		width: 70%;
 		height: 70%;
 		gap: 10px;
-		padding: 15px 20px;
 		border-radius: 15px;
 		box-shadow: 0px 0px 5px lightgrey;
+		overflow: hidden;
+	}
 
-		.clock {
-			width: 4rem;
+	aside {
+		min-width: 300px;
+		border-top-left-radius: 15px;
+		border-bottom-left-radius: 15px;
+		color: #808080;
+		background-color: #d5e5f5;
+		transition: all 500ms;
+
+		.btn-toggle-sidebar {
+			all: unset;
+			position: relative;
+			right: -105px;
+			top: 10px;
+			cursor: pointer;
+			transition: all 500ms;
+
+			:global(.cross) {
+				opacity: 1;
+			}
+
+			:global(.timer) {
+				opacity: 0;
+			}
 		}
+
+		&.hidden {
+			margin-left: -300px;
+
+			.btn-toggle-sidebar {
+				margin-left: 115px;
+
+				:global(.cross) {
+					opacity: 0;
+				}
+
+				:global(.timer) {
+					opacity: 1;
+				}
+			}
+		}
+
+		h3 {
+			margin: 15px;
+		}
+
+		ol {
+			display: flex;
+			flex-direction: column;
+			gap: 5px;
+			padding: 0px 25px;
+			margin: 0px;
+			width: calc(100% - 2 * 25px);
+			list-style-position: inside;
+			overflow-y: auto;
+		}
+	}
+
+	.time {
+		position: relative;
+		background-color: white;
+		padding: 5px 10px;
+		border-radius: 5px;
+
+		div {
+			position: absolute;
+			height: 100%;
+			top: 0px;
+			left: 30px;
+			display: flex;
+			align-items: center;
+			gap: 5px;
+		}
+	}
+
+	.clock {
+		position: absolute;
+		top: 15px;
+		right: 20px;
+		width: 4rem;
 	}
 </style>
-<script lang="ts">
-	import { onMount } from "svelte";
-	import Button from "../components/Button.svelte"
-
-	let formattedTime = "00:00:00";
-	let stopwatchMilliseconds = "000";
-	let currentTime = "";
-	let startTime: number;
-	let stopwatchStarted = false;
-
-	let times: {primary: string; secondary: string}[] = [
-		{
-			primary: "12:34:56",
-			secondary: "789",
-		},
-	];
-
-	const divMod = (dividend: number, divisor: number) => [Math.floor(dividend / divisor), dividend % divisor];
-
-	function formatTime(milliseconds: number) {
-		let hours, minutes, seconds;
-		[seconds, milliseconds] = divMod(milliseconds, 1000);
-		[minutes, seconds] = divMod(seconds, 60);
-		[hours, minutes] = divMod(minutes, 60);
-		const parts = [hours, minutes, seconds].map((part) => `${part}`.padStart(2, "0"));
-		formattedTime = parts.join(":");
-		stopwatchMilliseconds = `${milliseconds}`.padStart(3, "0");
-	}
-
-	function tickTime() {
-		const now = new Date();
-		currentTime = now.toLocaleTimeString([], {hour: "2-digit", minute: "2-digit", second: "2-digit"});
-		if (stopwatchStarted) {
-			const timeElapsed = now.getTime() - startTime;
-			formatTime(timeElapsed);
-		}
-		requestAnimationFrame(tickTime);
-	}
-
-	function toggleStopwatch() {
-		stopwatchStarted = !stopwatchStarted;
-		if (stopwatchStarted) {
-			startTime = new Date().getTime();
-		}
-	}
-
-	function handleTime() {
-		if (!stopwatchStarted) return;
-		const newTime = { primary: formattedTime, secondary: stopwatchMilliseconds };
-		times = [...times, newTime];
-	}
-
-	const deleteTime = (idxToDelete: number) => times = times.filter((_, idx) => idx !== idxToDelete);
-
-	onMount(() => {
-		tickTime();
-	});
-</script>
-<div class="root container">
-
-	<main>
-		<aside class={"container" + (times.length > 0 ? " visible" : "")}>
-			<h3>{times.length > 0 ? "Zapisane czasy" : ""}</h3>
-			<ol>
-				{#each times as time, idx}
-				<li class="time">
-						<b>{time.primary}</b>
-						<small>{time.secondary}</small>
-					<button on:click={() => deleteTime(idx)}>X</button>
-				</li>
-				{/each}
-			</ol>
-		</aside>
-		<div class="stopwatch container">
-			<div class="current-reading">
-				<b>{formattedTime}</b>
-				{#if formattedTime + stopwatchMilliseconds !== "00:00:00000"}
-					<small>{stopwatchMilliseconds}</small>
-				{/if}
-			</div>
-			<div class="buttons container">
-				<Button
-					on:click={toggleStopwatch}
-					className={stopwatchStarted ? "stop" : "start"}
-		  		label={stopwatchStarted ? "Stop" : "Start"}
-			  />
-				<Button on:click={handleTime} disabled={!stopwatchStarted} label="Pomiar" />
-		  </div>
-		</div>
-	  <b class="clock">{currentTime}</b>
-	</main>
-</div>
-
