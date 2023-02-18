2c5cd321a7c3225f7d20261e3241f24c6e51e3bc
Konrad (PC) <konrad@guzek.uk>
2023-02-18 19:49:18 +0100

    mobile responsiveness

--- _app/immutable/chunks/2-161b6818.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-75e0166b.js";export{t as component};
---
+++ _app/immutable/chunks/2-711aab09.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-965732f2.js";export{t as component};
