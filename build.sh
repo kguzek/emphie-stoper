3200f87a3c757e9f164734a3581668b61f0166bc
Konrad (PC) <konrad@guzek.uk>
2023-02-18 13:48:57 +0100

    update build script

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@ function create_prod_branch() {
   git switch --orphan prod && echo "Created new production branch."
 }
 
+if [ ! -d "./build" ]; then
+  echo "No build files to move."
+  exit 1
+fi
+
 git switch prod || create_prod_branch
+
+exit 0
 
 # Remove all previously tracked files
 git ls-files | xargs -r rm
@@ -10,7 +17,8 @@ git ls-files | xargs -r rm
 # But restore .gitignore from main branch
 git checkout main .gitignore
 
-mv build/* . && rmdir build && echo "Moved build files."
+mv build/* build/.[!.]* .
+rmdir build && echo "Moved build files." || exit 1
 
 git add . && git commit -m "build latest app version" || exit 1
 git push origin prod && echo "Successfully pushed new commit."
