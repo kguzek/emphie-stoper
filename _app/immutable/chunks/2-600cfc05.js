9e47405de595f063ad4b4824cb05a020c167802e
Konrad (PC) <konrad@guzek.uk>
2023-02-19 14:51:41 +0100

    deploy latest app version

---
+++ _app/immutable/chunks/2-600cfc05.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-7cff8002.js";export{t as component};
--- _app/immutable/chunks/2-ec697f67.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-46b97622.js";export{t as component};
